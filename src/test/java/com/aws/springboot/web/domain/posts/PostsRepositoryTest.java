d8cb441d045bb98806c713b1e4b423117007555b
SungHyuck <jhk139814@gmail.com>
2021-12-26 23:13:24 +0900

    Chapter3 Commit

---
+++ src/test/java/com/aws/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.aws.springboot.web.domain.posts;
+
+import com.aws.springboot.domain.posts.Posts;
+import com.aws.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        //given
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jhk139814@gmail.com")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
+}
